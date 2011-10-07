ecc36d9d5aeab70e6d3422d07d0245678d7b75d2
Mike Kruk <mike.kruk@mslgroup.com>
2011-10-07 15:24:09 -0400

    added all defaults

--- lib/defaults.php
+++ lib/defaults.php
@@ -35,11 +35,49 @@ group('deploy', function() {
   });
 
   desc("Complete deployment stack (1 and done)");
-  task('initial','app','deploy:setup','deploy:update', function($app) {
-    //$app->invoke('deploy:setup');
-    //$app->invoke('deploy:update');
-  });
+  task('initial','app','deploy:setup','deploy:update','deploy:wordpress','deploy:toolkit');
 });
 task('deploy','deploy:update');
+
+//db
+group('db', function() {
+  task('backup','db', function($app) {
+    
+  });
+
+  task('merge','db', function($app) {
+    
+  });
+});
+
+//wordpress uploads
+group('uploads', function() {
+  desc("Download uploads from environment")
+  task('pull','app', function($app) {
+    
+  });
+
+  desc("Place all local uploads into environment")
+  task('push','app', function($app) {
+    
+  });
+});
+
+//wordpress
+desc("Create and deploy wp-config.php for environment")
+task('wp_config','app', function($app) {
+  
+});
+
+desc("Wordpress task stack for local machine (1 and done)")
+task('wpify', function($app) {
+  
+});
+
+//local
+desc("Create default deployment config.yml for project")
+task('config', function($app) {
+  
+});
 
 ?>
