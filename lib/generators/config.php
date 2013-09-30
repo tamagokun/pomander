0a0a089f01d957fe22fbc63355b2f52dade668cc
Mike Kruk <tamagokun@gmail.com>
2013-09-30 08:57:39 -0400

    Update config.php
    making it clear what needs to be in deployment script when going to a remote server.

--- lib/generators/config.php
+++ lib/generators/config.php
@@ -2,23 +2,28 @@
 
 $env->repository('set your repository location here')
     ->url('set your application url here')
-		->deploy_to('set your application location on server')
-		->releases(true)
-		->keep_releases(5)
-		//->user('set your ssh user')
-		//->scm('set your scm. defaults to git')
-		//->revision('')
-		//
-		//->backup(true)
+    ->releases(true)
+    ->keep_releases(5)
+    //->user('set your ssh user')
+    //->scm('set your scm. defaults to git')
+    //->revision('')
+    //
+    //->backup(true)
 ;
 
-$env->app(array(             // Your application server(s) host or IP	
-	'your app-server here'
-));
+//
+// If you are deploying your application to a remote server:
+//
+//$env->deploy_to('set your application location on server');
+//$env->app(array(             // Your application server(s) host or IP	
+//	'your app-server here'
+//));
 
-$env->db(array(              // Your database server(s) host or IP
-	'your db-server here'
-));
+// If you are deploying your application to a remote server,
+// and using a database:
+//$env->db(array(              // Your database server(s) host or IP
+//	'your db-server here'
+//));
 
 // If your app uses a database uncomment this:
 //$env->database(array(
