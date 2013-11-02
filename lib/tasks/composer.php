952ff497cf8f8bc520dfc1887858dc370fe75438
Mike Kruk <tamagokun@gmail.com>
2013-11-02 15:27:01 -0400

    added an update task for composer

--- lib/tasks/composer.php
+++ lib/tasks/composer.php
@@ -2,11 +2,20 @@
 
 group('composer', function() {
 
-	desc("Install composer dependencies");
+	desc("Install dependencies with Composer");
 	task('install', function($app) {
 		if($app->env->composer === false) return;
 		run(array(
-			"([ -e 'composer.json' ] && which composer &>/dev/null) && composer install || echo '    composer.json not found'"
+			"([ -e 'composer.json' ] && which composer &>/dev/null)",
+			"composer install --optimize-autoloader || echo '    composer.json not found'"
+		));
+	});
+
+	task('update', function($app) {
+		if($app->env->composer === false) return;
+		run(array(
+			"([ -e 'composer.json' ] && which composer &>/dev/null)",
+			"composer update --optimize-autoloader || echo '    composer.json not found'"
 		));
 	});
 
