ea2c6efeeb1e3e3f89c5cc05a44accdf42af3704
Mike Kruk <tamagokun@gmail.com>
2014-01-06 10:12:43 -0500

    some initial Builder tests.

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -1,6 +1,62 @@
 <?php
 
+use \Pomander\Builder;
+
 class BuilderClass extends TestCase
 {
+  public function testDetectEnvironments()
+  {
+    $builder = $this->builder();
+    $builder->config();
+
+    $app = builder()->get_application();
+    $task_names = array_keys($app->get_tasks());
+    $this->assertSame(array('development', 'test'), $task_names);
+  }
+
+  public function testCanFirstRun()
+  {
+    $builder = $this->builder();
+    $builder->run();
+
+    $app = builder()->get_application();
+    $app->invoke('environment');
+
+    $this->expectOutputString("(in ".__DIR__.")\n");
+  }
+
+  public function testCanLoadEnvironments()
+  {
+    ob_start();
+    $builder = $this->builder();
+    $builder->run();
+    ob_end_clean();
+
+    $app = builder()->get_application();
+    // default environment is development
+    $app->invoke("testing");
+    $this->expectOutputString("hello");
+
+    // load test and check environment
+    ob_start();
+    $app->invoke("test");
+    ob_end_clean();
+    $this->assertSame("git@github.com:tamagokun/pomander.git", $app->env->repository);
+
+    // re-load development for sanity
+    ob_start();
+    $app->invoke("development");
+    ob_end_clean();
+    $app->invoke("testing");
+    $this->expectOutputString("hellohello");
+  }
+
+  protected function builder()
+  {
+    // clear tasks and change directory
+    chdir(__DIR__);
+    phake\Builder::$global = new phake\Builder;
+    return new Builder();
+  }
 
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/lib/Pomander.php';
 
 abstract class TestCase extends PHPUnit_Framework_TestCase
 {
