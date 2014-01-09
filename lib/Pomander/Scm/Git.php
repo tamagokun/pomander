e63272c94b91b9b9b9c9850719cf2988d619071c
Mike Kruk <tamagokun@gmail.com>
2014-01-09 11:34:24 -0500

    using git fetch before resets and checkouts.

--- lib/Pomander/Scm/Git.php
+++ lib/Pomander/Scm/Git.php
@@ -10,21 +10,24 @@ class Git extends \Pomander\Scm
 
     public function update()
     {
-        if (!empty($this->app->env->branch)) {
-            $cmd = "git reset --hard --quiet && git checkout {$this->app->env->branch} --quiet && git pull --quiet";
+        $cmd = array("git fetch --all --quiet");
+        $branch = isset($this->app["branch"])? $this->app["branch"] : $this->app->env->branch;
+
+        if (!empty($branch)) {
+            $cmd[] = "git reset --hard --quiet && git checkout {$branch} --quiet";
         } elseif (!empty($this->app->env->revision)) {
-            $cmd = "git reset --hard {$this->app->env->revision} --quiet";
+            $cmd[] = "git reset --hard {$this->app->env->revision} --quiet";
         } else {
-            $cmd = 'git reset --hard HEAD --quiet && git pull --quiet';
+            $cmd[] = 'git reset --hard HEAD --quiet';
         }
 
         if ($this->app->env->submodule !== false) {
-            $cmd .= ' && git submodule update --init --recursive --quiet';
+            $cmd[] = 'git submodule update --init --recursive --quiet';
         }
 
-        $cmd .= ' && git log --date=relative --format=format:"%C(bold blue)(%ar)%C(reset) // %an \'%s\' %C(bold green)(%h)%C(reset)" | head -1';
+        $cmd[] = 'git log --date=relative --format=format:"%C(bold blue)(%ar)%C(reset) %an \'%s\' %C(bold green)(%h)%C(reset)" | head -1';
 
-        return $cmd;
+        return implode(' && ', $cmd);
     }
 
     public function revision()
