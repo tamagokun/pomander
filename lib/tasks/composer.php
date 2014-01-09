b5202274fe0fd644e9116aa80629c53bea38b5f3
Léo POIROUX <leo.poiroux@gmail.com>
2014-01-10 00:58:44 +0100

    Composer disable by default

--- lib/tasks/composer.php
+++ lib/tasks/composer.php
@@ -4,7 +4,7 @@ group('composer', function () {
 
     desc("Install dependencies with Composer");
     task('install', function ($app) {
-        if($app->env->composer === false) return;
+        if($app->env->composer === true) return;
         info("composer", "install");
         run(array(
             "cd {$app->env->release_dir}",
@@ -14,7 +14,7 @@ group('composer', function () {
     });
 
     task('update', function ($app) {
-        if($app->env->composer === false) return;
+        if($app->env->composer === true) return;
         info("composer", "update");
         run(array(
             "cd {$app->env->release_dir}",
